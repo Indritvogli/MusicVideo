7eaa02e0f2203f11103829a2a10885eff2f33024
Indrit <ivogli00@gmail.com>
2016-02-08 12:34:43 -0500

    Completed Part 3

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -11,26 +11,50 @@ import Foundation
 class APIManager {
     
     func loadData(urlString:String , complition: (result:String) -> Void){
+        
        let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+       
         let session  = NSURLSession(configuration: config)
         
        // let session = NSURLSession.sharedSession()
         let url = NSURL(string: urlString)!
         
         let task = session.dataTaskWithURL(url) {
-            (data, response, error) -> Void in
-            dispatch_async(dispatch_get_main_queue()){
+             (data, response, error) -> Void in
+           
                 if error != nil {
+                     dispatch_async(dispatch_get_main_queue()) {
                     complition(result: (error!.localizedDescription))
+                    }
                 } else {
-                    complition(result: "NSURLSession Succesfull")
-                    print(data)
+                 
+                    //Added for JSONSerialization
+                    //print(data)
+                    do {
+                        
+                        
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as?[String: AnyObject] {
+                            
+                            print(json)
+                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                            dispatch_async(dispatch_get_global_queue(priority , 0)){
+                                dispatch_async(dispatch_get_main_queue()){
+                                complition(result: "JSONSerialization Succesfull")
+                            }
+                        }
+                    }
+            } catch {
+                dispatch_async(dispatch_get_main_queue()){
+                 complition(result: "error in NSJSerializations")
                 }
-                
             }
+            
+            //end of jsonSerialization
         }
-    
+    }
     task.resume()
-    
-          }
+}
+
+                
+                
 }
